7a98d6dc1dac1814fc1358a3fbe9a68c272b4283
Luis Correa <correaleyval@gmail.com>
2020-04-21 01:11:25 -0400

    /start Summary

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,27 @@
 import Telegraf from 'telegraf'
 
+import axios, { AxiosResponse } from 'axios'
+import summary from './types/summary'
+
 const bot = new Telegraf(process.env.BOT_TOKEN || '')
 
-bot.start(ctx => ctx.reply('I am live'))
+bot.start(async ctx => {
+    let res: AxiosResponse<summary> = 
+        await axios.get(process.env.API_URI + 'summary' || 'http://localhost:3000/summary')
+
+    ctx.replyWithMarkdown(`
+    🤒 **Diagnosticados**: ${res.data.total_diagnosticados}
+    🔬 **Diagnosticados hoy**: ${res.data.diagnosticados_hoy}
+    🤧 **Activos**: ${res.data.activos}
+    😃 **Recuperados**: ${res.data.total_recuperados}
+    🤩 **Índice de Recuperación**: ${res.data.recuperacion}%
+    ✈️ **Evacuados**: ${res.data.total_evacuados}
+    ⚰️ **Fallecidos**: ${res.data.total_fallecidos}
+    😵 **Mortalidad**: ${res.data.mortalidad}%
+    🏥 **Ingresados**: ${res.data.total_ingresados}
+    📆 **Actualizado**: ${res.data.fecha}
+    `)
+})
 
 bot.telegram.setWebhook(`${process.env.BOT_URI}/bot${process.env.BOT_TOKEN}`)
 
---
+++ src/types/summary.ts
@@ -0,0 +1,13 @@
+export default interface ISummary {
+    total_diagnosticados: number,
+    diagnosticados_hoy: number,
+    diferencia_ayer: number,
+    activos: number,
+    total_recuperados: number,
+    recuperacion: number,
+    total_evacuados: number,
+    total_fallecidos: number,
+    total_ingresados: number,
+    mortalidad: number,
+    fecha: string
+}
